879ebb79136898e2e40651a1e91c9b560cb046de
Jeff Hoather <jeffhoather@gmail.com>
2022-01-12 23:45:31 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/Calculate.js
@@ -0,0 +1,9 @@
+﻿
+
+
+$("#btnSend").click(function () {
+    alert("Your grade is: " +
+        ($("#assignmentGrade").val() + $("#groupProjectGrade").val() + $("#quizzesGrade").val()
+            + $("#examGrade").val() + $("#intexGrade").val())/5);
+})
+
